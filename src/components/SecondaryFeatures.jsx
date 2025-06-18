4537ff31a09af322508d3ea246d1caaad6455b0b
HarshitKhetawat <harshit.k@mysathi.in>
2025-06-18 17:20:18 +0530

    Save changes before pull

--- src/components/SecondaryFeatures.jsx
+++ src/components/SecondaryFeatures.jsx
@@ -1,11 +1,21 @@
 // components/SecondaryFeatures.jsx
-import { Activity, MapPin, Train } from 'lucide-react';
+import { Activity, MapPin, Train } from "lucide-react";
 
-const FeatureCard = ({ icon: Icon, title1, title2, bgColor, iconColor,customIcon, iconSrc }) => {
+const FeatureCard = ({
+  icon: Icon,
+  title1,
+  title2,
+  bgColor,
+  iconColor,
+  customIcon,
+  iconSrc,
+}) => {
   return (
     <button className="bg-white rounded-lg p-4 shadow-sm flex flex-col items-center space-y-2 hover:shadow-md transition-shadow">
-      <div className={`w-12 h-12 ${bgColor} rounded-full flex items-center justify-center`}>
-         {customIcon ? (
+      <div
+        className={`w-12 h-12 ${bgColor} rounded-full flex items-center justify-center`}
+      >
+        {customIcon ? (
           <img src={iconSrc} alt={`${title1} ${title2}`} className="w-6 h-6" />
         ) : (
           <Icon className={`w-6 h-6 ${iconColor}`} />
@@ -26,7 +36,7 @@ const SecondaryFeatures = () => {
       bgColor: "bg-orange-100",
       iconColor: "text-orange-600",
       customIcon: true,
-      iconSrc: "/logos/Running_Status.png" // Replace with your logo path
+      iconSrc: "/logos/Running_Status.png", // Replace with your logo path
     },
     {
       icon: MapPin,
@@ -35,7 +45,7 @@ const SecondaryFeatures = () => {
       bgColor: "bg-teal-100",
       iconColor: "text-teal-600",
       customIcon: true,
-      iconSrc: "/logos/Train_Search.png"
+      iconSrc: "/logos/Train_Search.png",
     },
     {
       icon: Train,
@@ -44,8 +54,8 @@ const SecondaryFeatures = () => {
       bgColor: "bg-indigo-100",
       iconColor: "text-indigo-600",
       customIcon: true,
-      iconSrc: "/logos/Train_Search.png"
-    }
+      iconSrc: "/logos/Train_Search.png",
+    },
   ];
 
   return (
